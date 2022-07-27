896fbfae2787b8902fcdf8773614437938c6ccdf
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-27 18:42:41 -0300

    Update script.js
    enougth for today

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const tmdbKey = "410cd8ed686e74f7848ed5a30daf3446";
-const tmdbBaseUrl =
-  "https://api.themoviedb.org/3/movie/550?api_key=410cd8ed686e74f7848ed5a30daf3446";
-const playBtn = document.getElementById("playBtn");
+const tmdbKey = '410cd8ed686e74f7848ed5a30daf3446';
+const tmdbBaseUrl = 'https://api.themoviedb.org/3/movie/550?api_key=410cd8ed686e74f7848ed5a30daf3446';
+const playBtn = document.getElementById('playBtn');
 
-const getGenres = () => {
-  genreRequestEndPoint = "/genre/movie/list";
+const getGenres = async () => {
+  const genreRequestEndPoint = '/genre/movie/list';
   const requestParams = `?api_key=${tmdbKey}`;
+  const urlToFetch = `${tmdbBaseUrl}${genreRequestEndpoint}${requestParams}`;
+
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok){
+      const jsonResponse = await response.json();
+      const genres = jsonResponse.genres;
+      return genres;
+    }
+  } catch (error){
+    console.log(error);
+  };
 };
 
 const getMovies = () => {
