3e9b04e71c47f323c7c577a5779ddf97da80b5c2
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2022-07-27 17:51:59 -0300

    work with API
    work with API

---
+++ script.js
@@ -0,0 +1,26 @@
+const tmdbKey = "410cd8ed686e74f7848ed5a30daf3446";
+const tmdbBaseUrl =
+  "https://api.themoviedb.org/3/movie/550?api_key=410cd8ed686e74f7848ed5a30daf3446";
+const playBtn = document.getElementById("playBtn");
+
+const getGenres = () => {
+  genreRequestEndPoint = "/genre/movie/list";
+  const requestParams = `?api_key=${tmdbKey}`;
+};
+
+const getMovies = () => {
+  const selectedGenre = getSelectedGenre();
+};
+
+const getMovieInfo = () => {};
+
+// Gets a list of movies and ultimately displays the info of a random movie from the list
+const showRandomMovie = () => {
+  const movieInfo = document.getElementById("movieInfo");
+  if (movieInfo.childNodes.length > 0) {
+    clearCurrentMovie();
+  }
+};
+
+getGenres().then(populateGenreDropdown);
+playBtn.onclick = showRandomMovie;
